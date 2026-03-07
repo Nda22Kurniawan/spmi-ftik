eb5dfb16ed88fb11d40914ab636fae7bd0ca7e43
Praditya Rendi <43976106+praditya19@users.noreply.github.com>
2026-03-07 15:50:53 +0700

    Fix casing of CekRole middleware class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cekRole' => \App\Http\Middleware\CekRole::class, // Ensure this class exists and is named correctly
+        'cekRole' => \App\Http\Middleware\cekRole::class, // Ensure this class exists and is named correctly
     ];
 }
